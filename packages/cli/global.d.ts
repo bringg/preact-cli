4683a72f1042c68f8ecdef107421535e2eaa9c8b
Ryan Christian <rchristian@ryanchristian.dev>
2022-07-08 06:12:13 -0500

    refactor: Misc edits to common deps & workspace helper scripts

--- packages/cli/global.d.ts
+++ packages/cli/global.d.ts
@@ -8,4 +8,13 @@ declare global {
 	}
 }
 
+// Modified from https://github.com/DefinitelyTyped/DefinitelyTyped/blob/master/types/shelljs/index.d.ts
+declare module 'shelljs' {
+	const shell: {
+		cd: (string) => void;
+		exec: (string) => { stdout: string; stderr: string; code: number };
+	};
+	export = shell;
+}
+
 export {};
